5e543e7909c41dccdc83a64b5ffcfafe79852e22
root <root@Bens-Desktop.localdomain>
2021-10-14 11:05:10 -0700

    add index.html app.js style.css

---
+++ app.js
@@ -0,0 +1,35 @@
+const form = document.getElementById('giphyform');
+const searchInput = document.getElementById('searchinput');
+const searchBtn = document.getElementById('searchbtn');
+const removeBtn = document.getElementById('removebtn');
+const imgDiv = document.getElementById('images');
+
+async function getImg(q) {
+    const giphyObject = {
+        q,
+        api_key: 'SWZZHqKLPdeAuZLcnRWZKu9sTteI0ded',
+    };
+    const res = await axios.get('https://api.giphy.com/v1/gifs/search', {
+        params: giphyObject,
+    });
+    const randNum = Math.floor(Math.random() * res.data.data.length);
+    const imgURL = res.data.data[randNum].images.original.url;
+    const newImg = document.createElement('img');
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('giphy');
+    imgContainer.append(newImg);
+    newImg.src = imgURL;
+    imgDiv.append(imgContainer);
+}
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (searchInput.value) {
+        getImg(searchInput.value);
+    }
+    searchInput.value = '';
+});
+
+removeBtn.addEventListener('click', function () {
+    imgDiv.innerHTML = '';
+});
